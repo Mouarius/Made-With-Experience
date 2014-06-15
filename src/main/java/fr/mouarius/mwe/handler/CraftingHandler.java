e307518c7717b44a4a936d1bcfcf1b51ea2ffa44
mouarius <mouarius@gmail.com>
2014-06-15 23:56:34 +0200

    Added the handler for recipes

---
+++ src/main/java/fr/mouarius/mwe/handler/CraftingHandler.java
@@ -0,0 +1,13 @@
+package fr.mouarius.mwe.handler;
+
+public class CraftingHandler {
+    public void init(){
+        registerRecipes();
+    }
+
+    private void registerRecipes()
+    {
+        //CraftingManager.getInstance().addRecipe();
+
+    }
+}
