222e8a222b124e0a0de13ee0d695f3e08f6d3e00
mouarius <mouarius@gmail.com>
2014-06-24 01:18:53 +0200

    Changed the version of my mod

--- src/main/java/fr/mouarius/mwe/reference/Reference.java
+++ src/main/java/fr/mouarius/mwe/reference/Reference.java
@@ -4,10 +4,10 @@ public class Reference {
 
     /* MOD REFERENCE */
     public static final String MOD_ID = "mwe";
-    public static final String VERSION = "beta_1.0";
+    public static final String MOD_NAME = "Made With Experience";
+    public static final String VERSION = "1.0";
     public static final String CLIENT_PROXY_LOCATION = "fr.mouarius.mwe.proxy.ClientProxy";
     public static final String SERVER_PROXY_LOCATION = "fr.mouarius.mwe.proxy.CommonProxy";
-    public static final String MOD_NAME = "Made With Experience";
 
 
 }
